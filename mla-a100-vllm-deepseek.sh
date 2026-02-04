b1850021c670519af05bb41735d23e4390ed8874
pengw00 <water685@hotmail.com>
2026-02-04 15:14:20 -0800

    use pre-compile file

--- mla-a100-vllm-deepseek.sh
+++ mla-a100-vllm-deepseek.sh
@@ -4,10 +4,14 @@ pip install --upgrade pip
 pip uninstall -y numpy
 pip install "numpy<2.0.0" vllm flash-attn --no-build-isolation
 
+# 访问 FlashAttention 的 GitHub Releases 页面找到对应 CUDA 和 Torch 版本的链接
+# 或者尝试强制寻找预编译包：
+pip install flash-attn --no-build-isolation --prefer-binary
 
 echo "正在安装 vLLM..."
 pip install vllm
 
+export VLLM_USE_PRECOMPILED=1
 export VLLM_LOGGING_LEVEL=DEBUG
 export RAY_LOG_TO_STDERR=1
 
